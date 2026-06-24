dcc067bedf26f27e0d7fcdf0082f9d07e3a75c08
18-anth <anthonycordova330@gmail.com>
2026-06-24 14:06:18 -0500

    config src

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -7,10 +7,11 @@ import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <Routes>
-        <Route path="/login" element={<LoginPanel />} />
-        <Route path="/register" element={<RegisterPanel />} />
-        <Route path="/dealer/:id" element={<Dealer/>} />
-        <Route path="/postreview/:id" element={<PostReview/>} />
+      Route
+      <Route path="/login" element={<LoginPanel />} />
+      <Route path="/register" element={<RegisterPanel />} />
+      <Route path="/dealer/:id" element={<Dealer />} />
+      <Route path="/postreview/:id" element={<PostReview />} />
     </Routes>
   );
 }
--- server/frontend/src/components/Register/Register.jsx
+++ server/frontend/src/components/Register/Register.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Header from "../Header/Header";
 import "./Register.css";
 
-const Register = () => {
+const Register = ({ onClose }) => {
 
   const [userName, setUserName] = useState("");
   const [firstName, setFirstName] = useState("");
@@ -40,60 +40,61 @@ const Register = () => {
   return (
     <div>
       <Header />
+      <div onClick={onClose}>
 
-      <div className="modalContainer">
-        <form className="login_panel" onSubmit={register}>
+        <div className="modalContainer">
+          <form className="login_panel" onSubmit={register}>
 
-          <div>
-            <span className="input_field">Username </span>
-            <input
-              type="text"
-              placeholder="Username"
-              className="input_field"
-              onChange={(e) => setUserName(e.target.value)}
-            />
-          </div>
+            <div>
+              <span className="input_field">Username </span>
+              <input
+                type="text"
+                placeholder="Username"
+                className="input_field"
+                onChange={(e) => setUserName(e.target.value)}
+              />
+            </div>
 
-          <div>
-            <span className="input_field">First Name </span>
-            <input
-              type="text"
-              placeholder="First Name"
-              className="input_field"
-              onChange={(e) => setFirstName(e.target.value)}
-            />
-          </div>
+            <div>
+              <span className="input_field">First Name </span>
+              <input
+                type="text"
+                placeholder="First Name"
+                className="input_field"
+                onChange={(e) => setFirstName(e.target.value)}
+              />
+            </div>
 
-          <div>
-            <span className="input_field">Last Name </span>
-            <input
-              type="text"
-              placeholder="Last Name"
-              className="input_field"
-              onChange={(e) => setLastName(e.target.value)}
-            />
-          </div>
+            <div>
+              <span className="input_field">Last Name </span>
+              <input
+                type="text"
+                placeholder="Last Name"
+                className="input_field"
+                onChange={(e) => setLastName(e.target.value)}
+              />
+            </div>
 
-          <div>
-            <span className="input_field">Password </span>
-            <input
-              type="password"
-              placeholder="Password"
-              className="input_field"
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
+            <div>
+              <span className="input_field">Password </span>
+              <input
+                type="password"
+                placeholder="Password"
+                className="input_field"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
 
-          <div>
-            <input
-              className="action_button"
-              type="submit"
-              value="Register"
-            />
-          </div>
+            <div>
+              <input
+                className="action_button"
+                type="submit"
+                value="Register"
+              />
+            </div>
 
-        </form>
-      </div>
+          </form>
+        </div> </div>
     </div>
   );
 };
