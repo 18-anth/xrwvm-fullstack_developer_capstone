3c8cf7c00689875778fe95da9316348659eb11d6
18-anth <anthonycordova330@gmail.com>
2026-06-26 12:46:20 -0400

    config server

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -69,31 +69,36 @@ def get_cars(request):
 # Create a `registration` view to handle sign up request
 @csrf_exempt
 def registration(request):
-    if request.method == "POST":
-        data = json.loads(request.body)
+    context = {}
 
-        username = data["userName"]
-        password = data["password"]
-        first_name = data["firstName"]
-        last_name = data["lastName"]
-        email = data["email"]
+    # Load JSON data from the request body
+    data = json.loads(request.body)
+    username = data['userName']
+    password = data['password']
+    first_name = data['firstName']
+    last_name = data['lastName']
+    email = data['email']
+    username_exist = False
+    email_exist = False
+    try:
+        # Check if user already exists
+        User.objects.get(username=username)
+        username_exist = True
+    except:
+        # If not, simply log this is a new user
+        logger.debug("{} is new user".format(username))
 
-        if User.objects.filter(username=username).exists():
-            return JsonResponse({"userName": username,
-                                 "error": "Already Registered"})
-
-        user = User.objects.create_user(
-            username=username,
-            password=password,
-            first_name=first_name,
-            last_name=last_name,
-            email=email,
-        )
-
+    # If it is a new user
+    if not username_exist:
+        # Create user in auth_user table
+        user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name,password=password, email=email)
+        # Login the user and redirect to list page
         login(request, user)
-
-        return JsonResponse({"userName": username, "status": "Authenticated"})
-
+        data = {"userName":username,"status":"Authenticated"}
+        return JsonResponse(data)
+    else :
+        data = {"userName":username,"error":"Already Registered"}
+        return JsonResponse(data)
 
 # # Update the `get_dealerships` view to render the index page with
 # a list of dealerships
