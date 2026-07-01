af76f6a1b65c10ab83fa6320ad157ec46a304acc
18-anth <anthonycordova330@gmail.com>
2026-07-01 12:45:02 -0400

    server/frontend

--- server/frontend/src/components/Dealers/Dealers.jsx
+++ server/frontend/src/components/Dealers/Dealers.jsx
@@ -5,6 +5,8 @@ import Header from '../Header/Header';
 import review_icon from "../assets/reviewicon.png"
 
 const Dealers = () => {
+    const [searchQuery, setSearchQuery] = useState('');
+    const [originalDealers, setOriginalDealers] = useState([]);
     const [dealersList, setDealersList] = useState([]);
     // let [state, setState] = useState("")
     let [states, setStates] = useState([])
@@ -21,8 +23,8 @@ const Dealers = () => {
         });
         const retobj = await res.json();
         if (retobj.status === 200) {
-            let state_dealers = Array.from(retobj.dealers)
-            setDealersList(state_dealers)
+            let state_dealers = Array.from(retobj.dealers);
+            setDealersList(state_dealers);
         }
     }
 
@@ -32,20 +34,34 @@ const Dealers = () => {
         });
         const retobj = await res.json();
         if (retobj.status === 200) {
-            let all_dealers = Array.from(retobj.dealers)
+            let all_dealers = Array.from(retobj.dealers);
             let states = [];
             all_dealers.forEach((dealer) => {
                 states.push(dealer.state)
             });
 
-            setStates(Array.from(new Set(states)))
-            setDealersList(all_dealers)
+            setStates(Array.from(new Set(states)));
+            setDealersList(all_dealers);
+            setOriginalDealers(all_dealers);
         }
     }
     useEffect(() => {
         get_dealers();
     }, []);
 
+    const handleInputChange = (event) => {
+        const query = event.target.value;
+        setSearchQuery(query);
+        const filtered = originalDealers.filter(dealer =>
+            dealer.state.toLowerCase().includes(query.toLowerCase())
+        );
+        setDealersList(filtered);
+    };
+    const handleLostFocus = () => {
+        if (!searchQuery) {
+            setDealersList(originalDealers);
+        };
+    };
 
     let isLoggedIn = sessionStorage.getItem("username") != null ? true : false;
     return (
@@ -60,13 +76,7 @@ const Dealers = () => {
                     <th>Address</th>
                     <th>Zip</th>
                     <th>
-                        <select name="state" id="state" onChange={(e) => filterDealers(e.target.value)}>
-                            <option value="" selected disabled hidden>State</option>
-                            <option value="All">All States</option>
-                            {states.map(state => (
-                                <option value={state}>{state}</option>
-                            ))}
-                        </select>
+                        <input type="text" placeholder="Search states..." onChange={handleInputChange} onBlur={handleLostFocus} value={searchQuery} />
 
                     </th>
                     {isLoggedIn ? (
